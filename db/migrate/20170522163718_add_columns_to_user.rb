d57eabb1c81fc0dd3b17a7648749048cd45fb8c2
Joanna Gaudyn <aska.gaudyn@gmail.com>
2017-05-22 17:46:25 +0100

    added validations to user model

---
+++ db/migrate/20170522163718_add_columns_to_user.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :username, :string
+    add_column :users, :description, :text
+    add_column :users, :photo, :string
+  end
+end
