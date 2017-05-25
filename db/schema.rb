1ddb31f7e7d32740ece3a42807e7b066aae05355
Viola Stoeckli <violastoeckli@hotmail.com>
2017-05-25 18:18:44 +0100

    Added column sku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524105633) do
+ActiveRecord::Schema.define(version: 20170525171747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170524105633) do
     t.datetime "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sku"
     t.index ["buyer_id"], name: "index_bookings_on_buyer_id"
     t.index ["equipment_id"], name: "index_bookings_on_equipment_id"
     t.index ["seller_id"], name: "index_bookings_on_seller_id"
